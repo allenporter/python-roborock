6650a5c8159d741e4c757f84c35ede7e86fdfb4c
Allen Porter <allen.porter@gmail.com>
2025-08-09 09:43:23 -0700

    feat: Update the cli cache to also store network info
    A follow up step will support caching network info for the new device
    manager API, and will use the CLI to do this.

--- roborock/cli.py
+++ roborock/cli.py
@@ -4,6 +4,7 @@ import logging
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Any
+from dataclasses import dataclass, field
 
 import click
 from pyshark import FileCapture  # type: ignore
