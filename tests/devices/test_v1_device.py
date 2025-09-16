ed5f7f7022cf23371a39e84ee8794d745daf99f2
Allen Porter <allen.porter@gmail.com>
2025-09-16 05:33:46 -0700

    chore: Add test coverage of end to end trait parsin from raw responses

--- tests/devices/test_v1_device.py
+++ tests/devices/test_v1_device.py
@@ -1,19 +1,29 @@
 """Tests for the Device class."""
 
+import pathlib
 from unittest.mock import AsyncMock, Mock
+from collections.abc import Awaitable, Callable
 
 import pytest
+from syrupy import SnapshotAssertion
 
 from roborock.containers import HomeData, S7MaxVStatus, UserData
 from roborock.devices.device import RoborockDevice
+from roborock.devices.traits.clean_summary import CleanSummaryTrait
+from roborock.devices.traits.dnd import DoNotDisturbTrait
+from roborock.devices.traits.sound_volume import SoundVolumeTrait
 from roborock.devices.traits.status import StatusTrait
 from roborock.devices.traits.trait import Trait
+from roborock.devices.v1_rpc_channel import decode_rpc_response
+from roborock.roborock_message import RoborockMessage, RoborockMessageProtocol
 
 from .. import mock_data
 
 USER_DATA = UserData.from_dict(mock_data.USER_DATA)
 HOME_DATA = HomeData.from_dict(mock_data.HOME_DATA_RAW)
 STATUS = S7MaxVStatus.from_dict(mock_data.STATUS)
+
+TESTDATA = pathlib.Path("tests/protocols/testdata/v1_protocol/")
 
 
 @pytest.fixture(autouse=True, name="channel")
@@ -45,7 +55,10 @@ def traits_fixture(rpc_channel: AsyncMock) -> list[Trait]:
         StatusTrait(
             product_info=HOME_DATA.products[0],
             rpc_channel=rpc_channel,
-        )
+        ),
+        CleanSummaryTrait(rpc_channel=rpc_channel),
+        DoNotDisturbTrait(rpc_channel=rpc_channel),
+        SoundVolumeTrait(rpc_channel=rpc_channel),
     ]
 
 
@@ -70,19 +83,45 @@ async def test_device_connection(device: RoborockDevice, channel: AsyncMock) -> 
     assert unsub.called
 
 
-async def test_device_get_status_command(device: RoborockDevice, rpc_channel: AsyncMock) -> None:
+@pytest.fixture(name="setup_rpc_channel")
+def setup_rpc_channel_fixture(rpc_channel: AsyncMock, payload: pathlib.Path) -> AsyncMock:
+    """Fixture to set up the RPC channel for the tests."""
+    # The values other than the payload are arbitrary
+    message = RoborockMessage(
+        protocol=RoborockMessageProtocol.GENERAL_RESPONSE,
+        payload=payload.read_bytes(),
+        seq=12750,
+        version=b"1.0",
+        random=97431,
+        timestamp=1652547161,
+    )
+    response_message = decode_rpc_response(message)
+    rpc_channel.send_command.return_value = response_message.data
+    return rpc_channel
+
+
+@pytest.mark.parametrize(
+    ("payload", "trait_name", "trait_type", "trait_method"),
+    [
+        (TESTDATA / "get_status.json", "status", StatusTrait, StatusTrait.get_status),
+        (TESTDATA / "get_dnd.json", "do_not_disturb", DoNotDisturbTrait, DoNotDisturbTrait.get_dnd_timer),
+        (TESTDATA / "get_clean_summary.json", "clean_summary", CleanSummaryTrait, CleanSummaryTrait.get_clean_summary),
+        (TESTDATA / "get_volume.json", "sound_volume", SoundVolumeTrait, SoundVolumeTrait.get_volume),
+    ]
+)
+async def test_device_trait_command_parsing(
+    device: RoborockDevice,
+    setup_rpc_channel: AsyncMock,
+    snapshot: SnapshotAssertion,
+    trait_name: str,
+    trait_type: type[Trait],
+    trait_method: Callable[..., Awaitable[object]],
+) -> None:
     """Test the device get_status command."""
-    # Mock response for get_status command
-    rpc_channel.send_command.return_value = [STATUS.as_dict()]
+    trait = device.traits[trait_name]
+    assert trait
+    assert isinstance(trait, trait_type)
+    result = await trait_method(trait)
+    assert setup_rpc_channel.send_command.called
 
-    # Test get_status and verify the command was sent
-    status_api = device.traits["status"]
-    assert isinstance(status_api, StatusTrait)
-    assert status_api is not None
-    status = await status_api.get_status()
-    assert rpc_channel.send_command.called
-
-    # Verify the result
-    assert status is not None
-    assert status.battery == 100
-    assert status.state == 8
+    assert result == snapshot
