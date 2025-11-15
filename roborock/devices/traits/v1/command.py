4fed92b6ec142285263eac50cd20996f8d38cf50
Allen Porter <allen.porter@gmail.com>
2025-11-15 09:35:43 -0800

    chore: Update working for the CommandTrait

--- roborock/devices/traits/v1/command.py
+++ roborock/devices/traits/v1/command.py
@@ -16,7 +16,13 @@ class CommandTrait:
         self._rpc_channel = None
 
     async def send(self, command: RoborockCommand | str, params: ParamsType = None) -> Any:
-        """Send a command to the device."""
+        """Send a command to the device.
+        
+        Sending a raw command to the device using this method does not update
+        the internal state of any other traits. It is the responsibility of the
+        caller to ensure that any traits affected by the command are refreshed
+        as needed.
+        """
         if not self._rpc_channel:
             raise ValueError("Device trait in invalid state")
         return await self._rpc_channel.send_command(command, params=params)
