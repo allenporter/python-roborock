faf6226ffca7122957107384e5d7fb94a2f6e33a
Allen Porter <allen.porter@gmail.com>
2026-01-26 20:37:53 -0800

    Add VacuumTrait to q10 devices

--- roborock/devices/traits/b01/q10/__init__.py
+++ roborock/devices/traits/b01/q10/__init__.py
@@ -1,12 +1,18 @@
 """Traits for Q10 B01 devices."""
 
+import asyncio
+import logging
 from typing import Any
 
-from roborock.devices.rpc.b01_q7_channel import send_decoded_command
+from roborock import B01Props
+from roborock.data.b01_q10.b01_q10_code_mappings import B01_Q10_DP
 from roborock.devices.traits import Trait
 from roborock.devices.transport.mqtt_channel import MqttChannel
 
 from .command import CommandTrait
+from .vacuum import VacuumTrait
+
+_LOGGER = logging.getLogger(__name__)
 
 __all__ = [
     "Q10PropertiesApi",
@@ -19,9 +25,15 @@ class Q10PropertiesApi(Trait):
     command: CommandTrait
     """Trait for sending commands to Q10 devices."""
 
+    vacuum: VacuumTrait
+    """Trait for sending Vacuum related commands to Q10 devices"""
+
     def __init__(self, channel: MqttChannel) -> None:
         """Initialize the B01Props API."""
         self.command = CommandTrait(channel)
+        self.vacuum = VacuumTrait(self.command)
+        self._channel = channel
+        self._task: asyncio.Task | None = None
 
 
 def create(channel: MqttChannel) -> Q10PropertiesApi:
